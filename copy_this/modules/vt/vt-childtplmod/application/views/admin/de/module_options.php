d2423f0f1104b262d6acef5913e9ef6e7423012f
Marat <m@marat.ws>
2017-08-07 14:03:58 +0200

    version 0.2.1

--- copy_this/modules/vt/vt-childtplmod/application/views/admin/de/module_options.php
+++
@@ -1,9 +0,0 @@
-<?php
-$sLangName  = "Deutsch";
-$style = '<style type="text/css">.groupExp a.rc b {font-size: medium; color: #ff3600; }.groupExp dt input.txt { width: 430px !important} .groupExp dl { display: block !important; } input.confinput {position: fixed; top: 20px; right: 70px; background: #008B2D; padding: 10px 25px; color: white; border: 1px solid black; cursor:pointer; font-size: 125%; } input.confinput:hover {outline: 3px solid #ff3600;} .groupExp dt textarea.txtfield {width: 430px; height: 150px;}</style>';
-$aLang = [
-    'charset'                               => 'UTF-8',
-    'SHOP_MODULE_GROUP_vtchildtplmod'    => $style.'Einstellungen',
-    'SHOP_MODULE_blVtctpmLogReplaced'    => 'log overriden templates',
-    'SHOP_MODULE_blVtctpmLogReplaceable' => 'log templates, that could be overriden'
-];
